0ab793cd4d7a21179b554e21fa0b9e662351e67f
Yannick Hold <yannickhold@gmail.com>
2015-04-22 09:54:43 -0400

    Added security module

---
+++ slc/security.py
@@ -0,0 +1,67 @@
+import os
+import string
+import errno
+import logging
+
+logger = logging.getLogger("slc")
+
+try:
+    import libnacl
+    import libnacl.utils
+except (ImportError, OSError):
+    logger = logging.getLogger("slc")
+    logger.warning("Could not load libnacl. Secure sockets won't be available.")
+
+
+class PossibleSecurityBreach(Exception):
+    pass
+
+
+jn = os.path.join
+slc_path = jn(os.path.expanduser('~'), '.slc')
+key_path = jn(slc_path, '0.0.0.0')
+
+def initializeSecurity():
+    global hostkey
+    # Check if user has a .slc directory to hold the keys
+    try:
+        os.makedirs(slc_path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    
+    # Create a secret key for the host if not already done
+    if not os.path.isfile(key_path):
+        logger.warning('First time using security. Building key for this host.')
+        hostkey = libnacl.public.SecretKey()
+        hostkey.save(key_path)
+    else:
+        hostkey = libnacl.utils.load_key(key_path)
+
+
+def stringToFilename(data):
+    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+    return ''.join(c for c in data if c in valid_chars)
+
+def getBox(remoteKey, target):
+    target = stringToFilename(str(target[0]))
+    print("remotekey:", remoteKey)
+    target_key_path = (jn(slc_path, target))
+    if os.path.isfile(target_key_path):
+        with open(target_key_path, 'rb') as fhdl:
+            key_already_here = fhdl.read()
+        if key_already_here != remoteKey:
+            raise PossibleSecurityBreach('The key sent by the peer is not the '
+                                         'same as the one already on the '
+                                         'system')
+    else:
+        with open(target_key_path, 'wb') as fhdl:
+            fhdl.write(remoteKey)
+        logger.warning('Added key for {} in wallet.'.format(target))
+
+    return libnacl.public.Box(hostkey.sk, remoteKey)
+
+def getOurPublicKey():
+    if 'hostkey' not in globals():
+        initializeSecurity()
+    return hostkey.pk
